045fdd208c1c21ab32938a5f77d8819aee7a45fc
nanared1 <abekah12345@gmail.com>
2022-03-18 08:08:13 -0230

    update table names in config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -10,36 +10,38 @@ dotenv.config();
 const rootDir = path.join(path.dirname(require.main?.filename), "../");
 
 export const Environments = {
-    production: "production",
-    development: "development",
-    test: "test",
+  production: "production",
+  development: "development",
+  test: "test",
 };
 
 export const config = {
-    rootServerDirectory: rootDir,
-    productionEnvironment: process.env.NODE_ENV === Environments.production,
-    testEnvironment: process.env.NODE_ENV === Environments.test,
-    developmentEnvironment: process.env.NODE_ENV === Environments.development,
-    environment: process.env.NODE_ENV || Environments.development,
-    security: {
-        privateKey: process.env.APP_SECURITY_PRIVATE_KEY || "anythingCanBeUsedHere",
-        tokenExpiry: process.env.NODE_ENV === Environments.production ? 3600 : 24 * 60 * 60,
-        verificationCodeExpiryInMinutes: 10,
-        jwtAlgorithm: "HS512",
-        saltRounds: 10,
-        issuer: "accounts.mediumclone.com",
-        audience: "mediumclone.com",
-        violationAlertsSentTo: "nbabekah@mun.ca",
-    },
-    tableNames: {
-        files: "files",
-        users: "users",
-    },
-    cors: {
-        origin: process.env.NODE_ENV === Environments.production ? "https://beapengine.ca" : "http://localhost:3000",
-    },
-    mongodb: {
-        host: "mongodb://127.0.0.1:27017",
-        dbName: "MediumClone",
-    },
+  rootServerDirectory: rootDir,
+  productionEnvironment: process.env.NODE_ENV === Environments.production,
+  testEnvironment: process.env.NODE_ENV === Environments.test,
+  developmentEnvironment: process.env.NODE_ENV === Environments.development,
+  environment: process.env.NODE_ENV || Environments.development,
+  security: {
+    privateKey: process.env.APP_SECURITY_PRIVATE_KEY || "anythingCanBeUsedHere",
+    tokenExpiry: process.env.NODE_ENV === Environments.production ? 3600 : 24 * 60 * 60,
+    verificationCodeExpiryInMinutes: 10,
+    jwtAlgorithm: "HS512",
+    saltRounds: 10,
+    issuer: "accounts.mediumclone.com",
+    audience: "mediumclone.com",
+    violationAlertsSentTo: "nbabekah@mun.ca",
+  },
+  tableNames: {
+    files: "files",
+    users: "users",
+    articles: "articles",
+    comments: "comments",
+  },
+  cors: {
+    origin: process.env.NODE_ENV === Environments.production ? "https://mediumclone.ca" : "http://localhost:3000",
+  },
+  mongodb: {
+    host: "mongodb://127.0.0.1:27017",
+    dbName: "MediumClone",
+  },
 };
